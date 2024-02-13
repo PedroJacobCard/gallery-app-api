ed9cf8fe1988c7d11a129749326c10b9758309b6
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2024-02-13 09:24:18 -0300

    added rimraf to devDependency and added dotenvConfig path  to find .env file

--- build/app/emails/mailer.js
+++ build/app/emails/mailer.js
@@ -1,4 +1,10 @@
 "use strict";
+
+const { configDotenv } = require("dotenv");
+
+configDotenv({
+  path: ['../../../.env', '.env'],
+})
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
