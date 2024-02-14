c8365ed79f12bf97231752e917ddf82dd758b195
Pedro Henrique Jacob Cardoso <pedrojacob05cardoso@gmail.com>
2024-02-14 16:06:54 -0300

    test development

--- build/app/config/allowsOrigin.js
+++ build/app/config/allowsOrigin.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const allowsOrigin = ['https://mygallery-m4nd.onrender.com'];
+const allowsOrigin = [
+  "https://mygallery-m4nd.onrender.com",
+  "http://localhost:5173",
+];
 exports.default = allowsOrigin;
